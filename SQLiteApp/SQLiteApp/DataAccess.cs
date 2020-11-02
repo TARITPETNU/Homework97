7557decdbf5bbdcf481a123a0aad8454f9083b13
Tarit.P <56666174+TARITPETNU@users.noreply.github.com>
2020-11-02 11:07:20 +0700

    Add homework 97

---
+++ SQLiteApp/SQLiteApp/DataAccess.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SQLiteApp
+{
+    class DataAccess
+    {
+
+        //Initialize the SQLite database
+        public static void InitializeDatabase()
+        {
+            using (SqliteConnection db =
+               new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS Customers (userID INTEGER PRIMARY KEY, " +
+                    "firstName NVARCHAR(20) NOT NULL, " +
+                    "lastName NVARCHAR(20) NOT NULL, " +
+                    "EMail NVARCHAR(20) NOT NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+
+        //Insert
+        public static void AddData(string inputText)
+        {
+           
+            using (SqliteConnection db =
+              new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @name);";
+                insertCommand.Parameters.AddWithValue("@name", inputText);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+
+        }
+
+        //Retrieve data from the SQLite database
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();
+
+          //  string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            using (SqliteConnection db =
+               new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT Text_Entry from MyTable", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                }
+
+                db.Close();
+            }
+
+            return entries;
+        }
+    }
+}
---
+++ SQLiteApp/SQLiteApp/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQLiteApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataAccess.InitializeDatabase();
+            DataAccess.AddData("Tarit");
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+           // DataAccess.AddData(inputTxt.ToString());
+        }
+
+        private void ShowBtn_Click(object sender, RoutedEventArgs e)
+        {
+           
+            string Data = "";
+            foreach (string inside in DataAccess.GetData())
+            {
+                Data = Data + " " + inside + "\n";
+            }
+            MessageBox.Show(Data);
+        }
+    }
+}
